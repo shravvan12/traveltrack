a864d0c74614829cce3a05340b3defbfea469f66
shravvan12 <shravankumar1745590@gmail.com>
2025-05-28 12:20:25 +0530

    Added form validation and updated styling

--- index.js
+++ index.js
@@ -27,8 +27,25 @@ app.get("/", async (req, res) => {
   });
   console.log(result.rows);
   res.render("index.ejs",{countries:countries,total: countries.length});
-  db.end()
 });
+
+app.post("/add",async(req,res)=>{
+  const input   = req.body["country"];
+  const result = await db.query(
+    "select country_code from countries where country_name = ($1) ",
+    [input]
+  );
+
+
+  if (result.rows.length!=0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)",
+     [countryCode]);
+     res.redirect("/");
+    }
+  });
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
